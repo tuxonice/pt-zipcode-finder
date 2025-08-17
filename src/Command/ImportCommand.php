54189a7ca1bc7c17e60543e07f36cf7304a22d91
Helder Correia <tuxboy@tlab.pt>
2025-08-17 21:06:43 +0100

    [CHORE] Set import command name

--- src/Command/ImportCommand.php
+++ src/Command/ImportCommand.php
@@ -3,6 +3,7 @@
 namespace Tlab\PtZipcodeFinder\Command;
 
 use RuntimeException;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -11,6 +12,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Tlab\PtZipcodeFinder\Importer\ZipcodeImporter;
 
+#[AsCommand(name: 'import')]
 class ImportCommand extends Command
 {
     private const DEFAULT_DATABASE_NAME = 'zipcodes';
